b01987cdd4e2861ffa28593830677a95832a306d
Simon B <simon.berda@gmail.com>
2015-01-01 16:43:18 +0100

    Modif common.php
    Ce fichier contient des fonctions pratiques qui seront utilisées un peu
    partout
    Nouvelles fonctions:
    getPdo();//Récupére un objet pdo
    encrypt($password);//Renvois un sha1 salé du mot de pass passé en param
    correspondant à l'id en param
    checkMail($mail);//Vérifie que le mail passé en param a la syntaxe de
    mail

--- common.php
+++ common.php
@@ -1,58 +1,38 @@
 <?php 
-
-	#Constant
-		#Connection
-			define("login", "monlogin");
-			define("password", "mypassword");
-			define("adress","mysql:host=localhost;dbname=transactions");
-		#Encryption
-			define("salt1","udsuyds");
-			define("salt2","ubvnvc");
-		#MailChecking
-			define("syntaxMail","#^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$#");
+//Connection
+define("LOGIN", "monlogin");
+define("PASSWORD", "mypassword");
+define("ADRESS","mysql:host=localhost;dbname=transactions");
+//Encryption
+define("salt1","udsuyds");
+define("salt2","ubvnvc");
+//MailChecking
+define("syntaxMail","#^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$#");
+//Function used to connect to the database
+function getPdo()
+{
+	$db = null;
+	try
+	{
+		$db = new PDO(ADRESS, LOGIN, PASSWORD);
+	}
+	catch(PDOException $e)
+	{
+		die($e->getMessage());
+	}
+	return $db;
 		
-	#Function used to connect to the database
-	function getPdo()
-	{
-		$db=null;
-		try
-		{
-			$db = new PDO(adress, login, password);
-		}
-		catch(PDOException $e)
-		{
-			die($e->getMessage());
-		}
-		return $db;
-		
-	}
-	
-	#function used to encrypt passwords
-	string encrypt($password)
-	{
-		return sha1(salt1.$password.salt2);
-	}
-	
-	
-	#Function used to get a user
-	function getUser($id_user)
-	{
-		$db = get_pdo(); #We connect to the database
-		$selected_user = $db->prepare
-		("SELECT username
-		  FROM user
-		  WHERE id_user = :id_user");
-		$selected_user->bindValue(':id_user',$id_user,PDO::PARAM_INT);
-		$selected_user->execute();
-		$user=$selected_user->fetch(PDO::FETCH_OBJ);#Ici on récupére un objet user
-		return $user
-	}
-	
-	#This function is used to check email syntax
-	function checkMail($email)
-	{
-		return	preg_match(syntaxMail,$email)
-	}
+}
+//function used to encrypt passwords
+encrypt($password)
+{
+	return sha1(salt1.$password.salt2);
+}	
+//This function is used to check email syntax
+function checkMail($email)
+{
+	return	preg_match(syntaxMail,$email);
+}
 
 
 ?>
