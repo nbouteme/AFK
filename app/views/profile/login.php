59938732e2fcc44b6ef63d3ff04cb722f07d6e10
ahmidou13 <mugiwaraakainu@gmail.com>
2015-01-16 11:55:28 +0100

    Mije à jour CSS
    login au milieu
    fond d'écran uniquement sur la page login
    correction action du formulaire, et le type du mot de passe

--- app/views/profile/login.php
+++ app/views/profile/login.php
@@ -1,14 +1,27 @@
-<div id="main_connexion_page">
-    <div id="form-login">
-        <form method="post" action="/login">
-            <h4>Connectez-vous</h4>
-            <input type="text" id="userName" class="form-control input-sm chat-input" placeholder="Nom d'utilisateur" />
-            <input type="text" id="userPassword" class="form-control input-sm chat-input" placeholder="Mot de passe" />
-            <div class="wrapper">
-                <span class="group-btn">     
-                    <input type="submit" value="Se connecter" class="btn btn-primary btn-md"/>
-                </span>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>AFK</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/assests/css/style.css">
+    </head>
+    <body id="bodyLogin">
+        <div id="main_connexion_page">
+            <div id="form-login">
+                <form method="post" action="<?php Url::to'/login'?>"
+                    <h4>Connectez-vous</h4>
+                    <input type="text" id="Name" class="form-control input-sm chat-input" placeholder="Nom d'utilisateur" />
+                    <input type="password" id="Password" class="form-control input-sm chat-input" placeholder="Mot de passe" />
+                    <div class="wrapper">
+                        <span class="group-btn">     
+                            <input type="submit" value="Se connecter" id="btnValider" class="btn btn-primary btn-md"/>
+                        </span>
+                    </div>
+                </form>
             </div>
-        </form>
-    </div>
-</div>
+        </div>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    </body>
+</html>
