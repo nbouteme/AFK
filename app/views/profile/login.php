e26c44cbd918eace8d12f81fb34dc3a666a4e7ee
ahmidou13 <mugiwaraakainu@gmail.com>
2015-01-16 09:14:15 +0100

    Update login.php & style.css
    Ajout du contenu

--- app/views/profile/login.php
+++ app/views/profile/login.php
@@ -1,5 +1,14 @@
-<form action="http://izanami.home/login" method="post">
-     <input type="text" name="pseudo"	/>Identifiant</br>
-     <input type="text" name="password"	/>Identifiant</br>
-     <input type="submit" name="mailer" value="Un jour je serai le meilleur dresseur">
-</form>
+<div id="main_connexion_page">
+    <div id="form-login">
+        <form method="post" action="/login">
+            <h4>Connectez-vous</h4>
+            <input type="text" id="userName" class="form-control input-sm chat-input" placeholder="Nom d'utilisateur" />
+            <input type="text" id="userPassword" class="form-control input-sm chat-input" placeholder="Mot de passe" />
+            <div class="wrapper">
+                <span class="group-btn">     
+                    <input type="submit" value="Se connecter" class="btn btn-primary btn-md"/>
+                </span>
+            </div>
+        </form>
+    </div>
+</div>
