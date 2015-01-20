3d09751062b73b53fdff8d423d13f015da44b25e
root <root@kuriyama.home>
2015-01-20 20:13:12 +0100

    Supprime des fichiers inutiles

--- app/views/profile/register.php
+++
@@ -1,9 +0,0 @@
-<form action="http://izanami.home/register" method="post">
-     <input type="text" name ="civilite"	/>Identifiant</br>
-     <input type="text" name ="email"		/>Identifiant</br>
-     <input type="text" name ="password"	/>Identifiant</br>
-     <input type="text" name ="pseudo"		/>Identifiant</br>
-     <input type="text" name ="nom"			/>Identifiant</br>
-     <input type="text" name ="prenom"		/>Identifiant</br>
-	 <input type="submit" name="mailer" value="Un jour je serai le meilleur dresseur">
-</form>
